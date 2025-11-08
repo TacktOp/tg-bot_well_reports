a1291044f730a47c3045c3bce0552ffe92fd8cba
TacktOp <slav.to@mail.ru>
2025-11-08 22:15:41 +0500

    update

--- handlers/add_report.py
+++ handlers/add_report.py
@@ -1,12 +1,12 @@
-from aiogram import types, Router, Bot
+from aiogram import types, Router
 from aiogram.filters.command import Command
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
+# from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 import datetime
 
-from database.database import db_add_report, db_get_employee, db_update_employee_report
+# from database.database import db_add_report, db_get_employee, db_update_employee_report
 
 add_report_router = Router()
 
@@ -15,7 +15,9 @@ class AddReport(StatesGroup):
     money = State()
     employee = State()
     bonuses = State()
+    info_bonuses = State()
     returns_money = State()
+    steam = State()
 
 
 @add_report_router.message(Command("addnewreport"))
@@ -30,6 +32,7 @@ async def cmd_add_report_money(message: types.Message, state: FSMContext):
         "2000 (нал терминал)\n"
         "3000 (безнал терминал)\n"
         "4000 (сбп терминал)\n"
+        "1000 (сбп планшет)\n"
     )
 
 
@@ -38,49 +41,66 @@ async def add_report_name(message: types.Message, state: FSMContext):
     await state.update_data(money=message.text)
     await state.set_state(AddReport.employee)
 
-    rows = db_get_employee()
-
-    builder = ReplyKeyboardBuilder()
-    for row in rows:
-        builder.add(types.KeyboardButton(text=row[0]))
-    builder.adjust(2)
+    # rows = db_get_employee()
+    #
+    # builder = ReplyKeyboardBuilder()
+    # for row in rows:
+    #     builder.add(types.KeyboardButton(text=row[0]))
+    # builder.adjust(2)
 
     await message.answer(
-        "Укажите админа",
-        reply_markup=builder.as_markup(resize_keyboard=True, one_time_keyboard=True)
-    )
+        "Укажите админа")
+    #     ,reply_markup=builder.as_markup(resize_keyboard=True, one_time_keyboard=True)
+    # )
 
 
 @add_report_router.message(AddReport.employee)
 async def add_report_returns(message: types.Message, state: FSMContext):
     await state.update_data(employee=message.text)
-    db_update_employee_report(message.text)
+    # db_update_employee_report(message.text)
     await state.set_state(AddReport.returns_money)
     await message.answer(
-        "Укажите возвраты за эвотор и лангейм\n\n"
+        "Укажите возвраты за эвотор И лангейм\n\n"
         "Пример:\n"
-        "123 (эвотор, если нет пиши 0)\n"
-        "123 (лангейм, если нет пиши 0)"
+        "100 ('эвотор', если нет пиши 0)\n"
+        "200 ('лангейм', если нет пиши 0)"
     )
 
 
 @add_report_router.message(AddReport.returns_money)
 async def add_report_bonuses(message: types.Message, state: FSMContext):
     await state.update_data(returns_money=message.text)
-    await  state.set_state(AddReport.bonuses)
+    await state.set_state(AddReport.bonuses)
     await message.answer(
-        "Укажите общее пополнение бонусов и количесво пополнений\n\n"
+        "Укажите пополнения бонусов\n\n"
         "Пример:\n"
-        "130\n"
-        "232\n"
-        "4343\n"
-        "(пополнения бонусов)"
+        "10\n"
+        "20\n"
+        "100\n"
     )
 
 
 @add_report_router.message(AddReport.bonuses)
-async def add_report(message: types.Message, state: FSMContext):
+async def add_report_info_bonuses(message: types.Message, state: FSMContext):
     await state.update_data(bonuses=message.text)
+    await state.set_state(AddReport.info_bonuses)
+    await message.answer(
+        "Укажите причину пополнение бонусов\n"
+        "Пример:\n"
+        "Обновление Фортнайт"
+    )
+
+
+@add_report_router.message(AddReport.info_bonuses)
+async def add_report_steam(message: types.Message, state: FSMContext):
+    await state.update_data(info_bonuses=message.text)
+    await state.set_state(AddReport.steam)
+    await message.answer("Укажите количество проданых аккаунтов Steam")
+
+
+@add_report_router.message(AddReport.steam)
+async def add_report(message: types.Message, state: FSMContext):
+    await state.update_data(steam=message.text)
     data = await state.get_data()
     await state.clear()
     moneys = f"{data['money']}".replace("\n", " ").replace(",", ".").split()
@@ -90,25 +110,34 @@ async def add_report(message: types.Message, state: FSMContext):
     bonuses_summ = 0
     for bonuses_i in bonuses:
         bonuses_x += 1
-        bonuses_summ += bonuses_i
+        bonuses_summ += int(bonuses_i)
+    info_bonuses = data['info_bonuses']
 
     returns = f"{data['returns_money']}".replace("\n", " ").split()
-    total = float(moneys[1]) + float(moneys[2]) + float(moneys[3]) + float(moneys[4]) + float(moneys[5])
+    if returns[1] is None:
+        returns[1] = '0'
+
+    steam = int(data['steam'])
+    steam_summ = steam * 150
+
+    total = float(
+        moneys[1]) + float(moneys[2]) + float(moneys[3]) + float(moneys[4]) + float(moneys[5]) + float(moneys[6])
+
     if datetime.datetime.now().hour >= 0:
-        db_add_report(
-            'день',
-            datetime.datetime.now().date(),
-            employee,
-            float(moneys[0]),
-            float(moneys[1]) - float(moneys[0]),
-            float(moneys[2]),
-            float(moneys[3]),
-            float(moneys[4]),
-            float(moneys[5]),
-            float(returns[0]),
-            float(returns[1]),
-            total
-        )
+        # db_add_report(
+        #     'день',
+        #     datetime.datetime.now().date(),
+        #     employee,
+        #     float(moneys[0]),
+        #     float(moneys[1]) - float(moneys[0]),
+        #     float(moneys[2]),
+        #     float(moneys[3]),
+        #     float(moneys[4]),
+        #     float(moneys[5]),
+        #     float(returns[0]),
+        #     float(returns[1]),
+        #     total
+        # )
 
         await message.answer(
             f"Дневная смена администратор:\n"
@@ -120,48 +149,57 @@ async def add_report(message: types.Message, state: FSMContext):
             f"Возврат {float(returns[0])}₽\n\n"
             f"Лангейм:\n"
             f"Нал {moneys[2]}₽\n\n"
-            f"Бонусы:\n"
-            f"Сумма {bonuses_summ}\n"
-            f"Кол-во пополнений {bonuses_x}\n\n"
             f"Терминал:\n"
             f"Нал {moneys[3]}₽\n"
             f"Безнал {moneys[4]}₽\n"
             f"СБП {moneys[5]}₽\n"
             f"Возврат {float(returns[1])}₽\n\n"
+            f"Планшет:\n"
+            f"СБП {moneys[6]}"
+            f"Бонусы:\n"
+            f"Сумма {bonuses_summ}\n"
+            f"Кол-во пополнений {bonuses_x}\n\n"
+            f"Причина: {info_bonuses}"
             f"Возвраты: {float(returns[0]) + float(returns[1])}₽\n"
             f"Итого: {total}₽"
         )
 
-    elif datetime.datetime.now().hour >= 9 and datetime.datetime.now().hour <= 11:
-        db_add_report(
-            'ночь',
-            datetime.datetime.today().date() - datetime.timedelta(days=1),
-            employee,
-            float(moneys[0]),
-            float(moneys[1]) - float(moneys[0]),
-            float(moneys[2]),
-            float(moneys[3]),
-            float(moneys[4]),
-            float(moneys[5]),
-            float(returns[0]),
-            float(returns[1]),
-            total
-        )
+    elif 9 <= datetime.datetime.now().hour <= 11:
+        # db_add_report(
+        #     'ночь',
+        #     datetime.datetime.today().date() - datetime.timedelta(days=1),
+        #     employee,
+        #     float(moneys[0]),
+        #     float(moneys[1]) - float(moneys[0]),
+        #     float(moneys[2]),
+        #     float(moneys[3]),
+        #     float(moneys[4]),
+        #     float(moneys[5]),
+        #     float(returns[0]),
+        #     float(returns[1]),
+        #     total
+        # )
         await message.answer(
             f"Ночная смена администратор:\n"
             f"{employee}\n\n"
-            f"{datetime.datetime.today().date() - datetime.timedelta(days=1)}\n\n"
+            f"{datetime.datetime.now().date()}\n\n"
             f"Эватор:\n"
             f"Нал {moneys[0]}₽\n"
             f"Безнал {float(moneys[1]) - float(moneys[0])}₽\n"
             f"Возврат {float(returns[0])}₽\n\n"
             f"Лангейм:\n"
-            f"Нал {moneys[2]}₽\n"
+            f"Нал {moneys[2]}₽\n\n"
             f"Терминал:\n"
             f"Нал {moneys[3]}₽\n"
             f"Безнал {moneys[4]}₽\n"
-            f"СБП {moneys[5]}₽\n\n"
+            f"СБП {moneys[5]}₽\n"
             f"Возврат {float(returns[1])}₽\n\n"
+            f"Планшет:\n"
+            f"СБП {moneys[6]}"
+            f"Бонусы:\n"
+            f"Сумма {bonuses_summ}\n"
+            f"Кол-во пополнений {bonuses_x}\n\n"
+            f"Причина: {info_bonuses}"
             f"Возвраты: {float(returns[0]) + float(returns[1])}₽\n"
             f"Итого: {total}₽"
         )
--- main.py
+++ main.py
@@ -1,70 +1,70 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import CommandStart
-import sqlite3
+# import sqlite3
 
 import asyncio
 import logging
 
 from config_reader import config
 from handlers.add_report import add_report_router
-from handlers.get_report_money import get_report_money_router
+# from handlers.get_report_money import get_report_money_router
 from handlers.add_employee import add_employee_router
 from handlers.delete_employee import delete_employee_router
-from handlers.get_employee_report import get_employee_report_router
+# from handlers.get_employee_report import get_employee_report_router
 from handlers.add_employee_quests import add_employee_quests_router
 
 logging.basicConfig(level=logging.INFO)
-connection = sqlite3.connect("./database/database.db")
-cursor = connection.cursor()
+# connection = sqlite3.connect("./database/database.db")
+# cursor = connection.cursor()
 bot = Bot(token=config.bot_token.get_secret_value())
 dp = Dispatcher()
 
 dp.include_router(add_report_router)
 dp.include_router(add_employee_router)
-dp.include_router(get_report_money_router)
+# dp.include_router(get_report_money_router)
 dp.include_router(delete_employee_router)
-dp.include_router(get_employee_report_router)
+# dp.include_router(get_employee_report_router)
 dp.include_router(add_employee_quests_router)
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS Reports (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    тип_смены type TEXT NOT NULL,
-    дата type DATE NOT NULL,
-    админ type TEXT,
-    эвотор_нал type FLOAT NOT NULL,
-    эвотор_безнал type FLOAT NOT NULL,
-    лангейм_нал type FLOAT NOT NULL,
-    терминал_нал type FLOAT NOT NULL,
-    терминал_безнал type FLOAT NOT NULL,
-    терминал_сбп type FLOAT NOT NULL,
-    возврат_эвотор type FLOAT ,
-    возврат_лангейм type FLOAT ,
-    итого type FLOAT NOT NULL
-)
-""")
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS Employees (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    тип_смены type TEXT NOT NULL,
-    имя type TEXT NOT NULL,
-    номер_телефона type INTEGER 
-)
-""")
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS EmployeesReports (
-    id INTEGER  PRIMARY KEY AUTOINCREMENT,
-    админ type TEXT NOT NULL,
-    количество_смен type INTEGER,
-    дата type DATE,
-    задачи type TEXT
-)
-""")
-
-connection.commit()
-connection.close()
+# cursor.execute("""
+# CREATE TABLE IF NOT EXISTS Reports (
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     тип_смены type TEXT NOT NULL,
+#     дата type DATE NOT NULL,
+#     админ type TEXT,
+#     эвотор_нал type FLOAT NOT NULL,
+#     эвотор_безнал type FLOAT NOT NULL,
+#     лангейм_нал type FLOAT NOT NULL,
+#     терминал_нал type FLOAT NOT NULL,
+#     терминал_безнал type FLOAT NOT NULL,
+#     терминал_сбп type FLOAT NOT NULL,
+#     возврат_эвотор type FLOAT ,
+#     возврат_лангейм type FLOAT ,
+#     итого type FLOAT NOT NULL
+# )
+# """)
+#
+# cursor.execute("""
+# CREATE TABLE IF NOT EXISTS Employees (
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     тип_смены type TEXT NOT NULL,
+#     имя type TEXT NOT NULL,
+#     номер_телефона type INTEGER
+# )
+# """)
+#
+# cursor.execute("""
+# CREATE TABLE IF NOT EXISTS EmployeesReports (
+#     id INTEGER  PRIMARY KEY AUTOINCREMENT,
+#     админ type TEXT NOT NULL,
+#     количество_смен type INTEGER,
+#     дата type DATE,
+#     задачи type TEXT
+# )
+# """)
+#
+# connection.commit()
+# connection.close()
 
 @add_report_router.message(CommandStart())
 async def cmd_start(message: types.Message):
